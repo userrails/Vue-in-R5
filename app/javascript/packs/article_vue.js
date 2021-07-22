2fbb0183cdd71b40b05b51a7729f1f7adcff308e
Shiv Raj Badu <shivrajbadu@gmail.com>
2021-07-22 13:07:28 +0545

    load vue js on particular section only and load with turbolinks

--- app/javascript/packs/article_vue.js
+++ app/javascript/packs/article_vue.js
@@ -5,37 +5,39 @@ import axios from 'axios'
 Vue.use(TurbolinksAdapter)
 
 document.addEventListener('turbolinks:load', () => {
-  var app7 = new Vue({
-    el: '#article-form',
-    data: {
-      form: {
-        title: '',
-        description: '',
+  if (document.querySelector("#article-form") != null) {
+    var app7 = new Vue({
+      el: '#article-form',
+      data: {
+        form: {
+          title: '',
+          description: '',
+        }
       }
-    }
-  })
-
-  var article_list = new Vue({
-    el: '#article-list',
-    data: {
-      articles: [],
-      keyword: '',
-    },
-    created() {
-      console.log('created')
-    },
-    mounted() {
-      axios.get('/articles.json')
-        .then(result => this.articles = result.data)
-        .catch(error => console.log(error))
-    },
-    methods: {
-      search() {
-        axios.get(`/articles.json?keyword=${this.keyword}`)
+    })
+  }
+  if (document.querySelector("#article-list") != null) {
+    var article_list = new Vue({
+      el: '#article-list',
+      data: {
+        articles: [],
+        keyword: '',
+      },
+      created() {
+        console.log('created')
+      },
+      mounted() {
+        axios.get('/articles.json')
           .then(result => this.articles = result.data)
           .catch(error => console.log(error))
+      },
+      methods: {
+        search() {
+          axios.get(`/articles.json?keyword=${this.keyword}`)
+            .then(result => this.articles = result.data)
+            .catch(error => console.log(error))
+        }
       }
-    }
-  })
-
+    })
+  }
 })
